795a52f1dd20955475edfc0814dd05b43af4e132
hansgunz <hans.gonzalez.25.07@gmail.com>
2025-08-20 16:20:27 -0600

    ubase url: update url.

--- app/src/main/java/com/example/lyricsapp/di/Const.kt
+++ app/src/main/java/com/example/lyricsapp/di/Const.kt
@@ -2,5 +2,5 @@ package com.example.lyricsapp.di
 
 object Const {
 
-    val BASE_URL = "https://private-anon-d0dd0b680f-lyricsovh.apiary-proxy.com/v1/"
+    val BASE_URL = "https://api.lyrics.ovh/v1/"
 }
